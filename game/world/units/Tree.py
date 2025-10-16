815583343b774b1c874320a01f506a44a57df11b
abmize <abmize@ncsu.edu>
2025-10-16 17:47:08 -0400

    Implemented provinces and gravestones

--- game/world/units/Tree.py
+++ game/world/units/Tree.py
@@ -10,6 +10,14 @@ class Tree(Unit):
     on tiles with trees, though soldier can be placed on them which has the same effect
     as if the tree was chopped down by a soldier moving onto the tile.
     Trees will randomly grow on empty tiles adjacent to other trees that are not water tiles.
+    Gravestones are a special type of tree that spawn when a soldier dies due to insufficient
+    resources to pay its upkeep. Gravestones can be removed like how trees can be chopped down,
+    but they do not provide any resources when removed. After 1 turn, a gravestone will
+    turn into a normal tree.
     """
-    def __init__(self, owner=None):
-        super().__init__(unitType="tree", attackPower=0, defensePower=0, upkeep=1, cost=0, canMove=False, owner=owner)
+    def __init__(self, isGravestone=False, owner=None):
+        if isGravestone:
+            super().__init__(unitType="gravestone", attackPower=0, defensePower=0, upkeep=0, cost=0, canMove=False, owner=owner)
+        else:
+            super().__init__(unitType="tree", attackPower=0, defensePower=0, upkeep=1, cost=0, canMove=False, owner=owner)
+        
