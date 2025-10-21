1d51f9959d020aad826829c84e999d8d56884aa2
DirusLupito <dirus.lupito@gmail.com>
2025-10-21 12:44:05 -0400

    Started working on an additional view for the grid

--- game/world/factions/Faction.py
+++ game/world/factions/Faction.py
@@ -7,4 +7,25 @@ class Faction:
         self.name = name  # String, Name of the faction
         self.color = color  # String, Color of the faction (e.g. 'red', 'blue', etc.)
         self.provinces = provinces if provinces is not None else []  # List of Province instances controlled by the faction
-    
+
+    def printWithFactionColor(self, text):
+        """
+        Prints the given text in the faction's color,
+        if the given color matches an ANSI color code.
+        """
+        color_codes = {
+            'red': '\033[91m',
+            'green': '\033[92m',
+            'yellow': '\033[93m',
+            'blue': '\033[94m',
+            'magenta': '\033[95m',
+            'cyan': '\033[96m',
+            'white': '\033[97m',
+            'reset': '\033[0m'
+        }
+        # If the faction's color is a valid ANSI color, use it
+        if self.color in color_codes:
+            print(f"{color_codes[self.color]}{text}{color_codes['reset']}")
+        else:
+            # Otherwise, print normally
+            print(text)
